86d0115ee57a2f37347f71af0f693ee2478ef24b
fernando.goncalez <fernando.goncalez@autotrac.com.br>
2019-05-02 15:17:31 -0300

    Testes .net framework bot

---
+++ Bot1/Bot1/Dialogs/GreetingDialog.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.Bot.Builder.Dialogs;
+using Microsoft.Bot.Connector;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace Bot1.Dialogs
+{
+    [Serializable]
+    public class GreetingDialog : IDialog
+    {
+        public Task StartAsync(IDialogContext context)
+        {
+            context.PostAsync("Hi I´m Feliu Bot");
+
+            context.Wait(MessageReceivedAsync);
+
+            return Task.CompletedTask;
+        }
+
+        public virtual async Task MessageReceivedAsync(IDialogContext context, IAwaitable<IMessageActivity> result)
+        {
+            var message = await result;
+            var userName = String.Empty;
+            var getName = false;
+
+            context.UserData.TryGetValue<string>("Name", out userName);
+            context.UserData.TryGetValue<bool>("GetName", out getName);
+
+            if(getName)
+            {
+                userName = message.Text;
+                context.UserData.SetValue<string>("Name", userName);
+                context.UserData.SetValue<bool>("GetName", false);
+            }
+
+            if(string.IsNullOrEmpty(userName))
+            {
+                await context.PostAsync("Whats is your name?");
+                context.UserData.SetValue<bool>("GetName", true);
+            }
+            else
+            {
+                await context.PostAsync($"Hi {userName}. How can I help you today?");
+            }
+
+            context.Wait(MessageReceivedAsync);
+        }
+    }
+}
---
+++ Bot1_Maratona/Bot1_Maratona/Dialogs/RootDialog.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.Bot.Builder.Dialogs;
+using Microsoft.Bot.Connector;
+
+namespace Bot1_Maratona.Dialogs
+{
+    [Serializable]
+    public class RootDialog : IDialog<object>
+    {
+        public Task StartAsync(IDialogContext context)
+        {
+            context.Wait(MessageReceivedAsync);
+
+            return Task.CompletedTask;
+        }
+
+        private async Task MessageReceivedAsync(IDialogContext context, IAwaitable<object> result)
+        {
+            var activity = await result as Activity;
+
+            // Calculate something for us to return
+            //int length = (activity.Text ?? string.Empty).Length;
+            // Return our reply to the user
+            //await context.PostAsync($"You sent {activity.Text} which was {length} characters");
+
+            await context.PostAsync("Olá, tudo bem?");
+
+            var message = activity.CreateReply();
+
+            var heroCard = new HeroCard()
+            {
+                Title = "Titulo 1",
+                Subtitle = "Subtitle",
+                Images = new List<CardImage>
+                {
+                    new CardImage("https://cdn.technologyadvice.com/wp-content/uploads/2018/02/friendly-chatbot-700x408.jpg",
+                    "Imagem de Bot")
+                }
+            };
+
+            message.Attachments.Add(heroCard.ToAttachment());
+
+            await context.PostAsync(message);
+
+            context.Wait(MessageReceivedAsync);
+        }
+    }
+}
